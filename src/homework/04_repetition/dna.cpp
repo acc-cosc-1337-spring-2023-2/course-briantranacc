d9dd3be257b981e63c842be2d3307bdeda9fea62
briantranacc <123131879+briantranacc@users.noreply.github.com>
2023-02-26 06:07:36 +0000

    Homework 4

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,39 @@
 //add include statements
+#include "dna.h"
 
 //add function(s) code here
+int factorial(int num)
+{
+    int result = 1;
+
+    for(int i = 1; i <= num; i++)
+    {
+        result *= i;
+    }
+
+    return result;
+}
+
+int gcd(int num1, int num2)
+{
+    int lowerValue, result;
+    
+    if (num1 < num2)
+    {
+        lowerValue = num1;
+    }
+    else
+    {
+        lowerValue = num2;
+    }
+
+    for (int i = 1; i <= lowerValue; i++)
+    {
+        if (num1 % i == 0 && num2 % i == 0)
+        {
+            result = i;
+        }
+    }
+
+    return result;
+}
--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,7 +1,8 @@
 //write include statements
+#include "dna.h"
 
 //write using statements
-
+using std::cout; using std::cin;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +13,46 @@ user enters a y or Y.
 */
 int main() 
 {
+	int choice, num, num1, num2;
+	auto exitProgram = 'n';
+
+	do
+	{
+		cout<<"1-Factorial\n";
+		cout<<"2-Greatest Common Divisor\n";
+		cout<<"3-Exit\n\n";
+		cout<<"Enter your choice: ";
+        cin>>choice;
+		cout<<"\n";
+
+		if (choice == 1)
+		{
+			cout<<"You chose Factorial.\n";
+			cout<<"Enter a number: "; 
+			cin>>num;
+			cout<<"The factorial of "<<num<<" is "<<factorial(num)<<"\n\n";
+		}
+		else if (choice == 2)
+		{
+			cout<<"You chose Greatest Common Divisor.\n";
+			cout<<"Enter the first number: ";
+			cin>>num1;
+			cout<<"Enter the second number: ";
+			cin>>num2;
+			cout<<"The Greatest Common Divisor is "<<gcd(num1, num2)<<"\n\n";
+		}
+		else if (choice == 3)
+		{
+			cout<<"Are you sure you want to exit? (enter y to exit): ";
+			cin>>exitProgram;
+		}
+		else
+		{
+			cout<<"Invalid option.\n\n";
+		}
+		
+	}
+	while(exitProgram != 'y');
+
 	return 0;
 }
