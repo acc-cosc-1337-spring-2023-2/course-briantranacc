ca5d093b2ab5b59ca0fbf9046b0836bf7ce52a6d
briantranacc <123131879+briantranacc@users.noreply.github.com>
2023-04-13 20:49:36 +0000

    ArrayList

--- src/examples/10_arrays/04_arrays_pointers/arrays_pointers.cpp
+++ src/examples/10_arrays/04_arrays_pointers/arrays_pointers.cpp
@@ -1,1 +1,61 @@
 //cpp
+#include "arrays_pointers.h"
+
+ArrayList::ArrayList(int* list, const int capacity) : elements(list)
+{
+    this->capacity = capacity;
+}
+
+void ArrayList::Add(int value)
+{
+    if(current_index < capacity)
+    {
+        elements[current_index] = value;
+        current_index++;
+    }
+}
+
+int ArrayList::Get_First_Index_Of(int value)
+{
+    auto index = -1;
+    auto local_current_index = 0;
+
+    while(local_current_index < current_index)
+    {
+        if(elements[local_current_index] == value)
+        {
+            index = local_current_index;
+            break;
+        }
+
+        local_current_index++;
+    }
+
+    return index;
+}
+
+void ArrayList::Delete(int value)
+{
+    auto index = Get_First_Index_Of(value);
+
+    if(index != -1)
+    {
+        AdjustElements(index);
+    }
+}
+
+void ArrayList::AdjustElements(int index)
+{
+    if(index == current_index-1)
+    {
+        current_index--;
+        return;
+    }
+
+    for(; index < current_index; index++)
+    {
+        elements[index] = elements[index+1];
+    }
+
+    current_index--;    
+}
