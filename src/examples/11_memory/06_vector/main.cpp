d8264b3c9af315d0ac03de10b7814b97ff572635
briantranacc <123131879+briantranacc@users.noreply.github.com>
2023-04-20 23:23:44 +0000

    lvalue and rvalue

--- src/examples/11_memory/06_vector/main.cpp
+++ src/examples/11_memory/06_vector/main.cpp
@@ -6,7 +6,8 @@ int main()
     //std::cout<<"load to the stack\n";
     use_vector();
     //std::cout<<"unloaded from the stack\n";
-
+    int num = 5;
+    int& num_ref = num;
     
 
     return 0;
